77ee647fe11085f1f9839f1f15eebdc62a91bb0b
Amy Cohen <anbcohen@gmail.com>
2018-08-18 14:22:31 -0700

    changed the height to wrap_content so the text shows up

--- app/src/main/java/com/example/a28_filterable_lists/DetailActivity.java
+++ app/src/main/java/com/example/a28_filterable_lists/DetailActivity.java
@@ -1,21 +1,14 @@
 package com.example.a28_filterable_lists;
 
-import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
-//    public TextView name;
-//    public TextView category;
-//    public TextView type;
-//    public TextView description;
     public Button goBackButton;
 
     @Override
@@ -33,11 +26,6 @@ public class DetailActivity extends AppCompatActivity {
                     .add(R.id.container, new DetailFragment())
                     .commit();
         }
-
-//        name = findViewById(R.id.name_detail);
-//        category = findViewById(R.id.category_detail);
-//        type = findViewById(R.id.type_detail);
-//        description = findViewById(R.id.description_detail);
 
         goBackButton = findViewById(R.id.goBack);
         goBackButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/a28_filterable_lists/DetailFragment.java
+++ app/src/main/java/com/example/a28_filterable_lists/DetailFragment.java
@@ -2,29 +2,15 @@ package com.example.a28_filterable_lists;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class DetailFragment extends Fragment{
 
 
-//    public TextView name;
-//    public TextView category;
-//    public TextView type;
-//    public TextView description;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//
-//        name = findViewById(R.id.name_detail);
-//        category = findViewById(R.id.category_detail);
-//        type = findViewById(R.id.type_detail);
-//        description = findViewById(R.id.description_detail);
-//
         View view = inflater.inflate(R.layout.activity_detail, container, false);
         return view;
 
