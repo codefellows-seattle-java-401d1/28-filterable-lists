77fd03d8792d56eb19c5959cf09dd1fc8de40f31
Amy Cohen <anbcohen@gmail.com>
2018-08-18 12:56:22 -0700

    added a fragment but nothing is populated yet

---
+++ app/src/main/java/com/example/a28_filterable_lists/DetailFragment.java
@@ -0,0 +1,38 @@
+package com.example.a28_filterable_lists;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DetailFragment extends Fragment{
+
+
+//    public TextView name;
+//    public TextView category;
+//    public TextView type;
+//    public TextView description;
+//    public Button goBackButton;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_go_back);
+//
+//        name = findViewById(R.id.name_detail);
+//        category = findViewById(R.id.category_detail);
+//        type = findViewById(R.id.type_detail);
+//        description = findViewById(R.id.description_detail);
+//
+//        goBackButton = findViewById(R.id.goBack);
+
+
+        View view = inflater.inflate(R.layout.activity_detail, container, false);
+        return view;
+
+    }
+}
