aea6018343535bf584d6f8c490939a5b89d92e7c
Suzanne Richman <suzanne@favoredfortune.com>
2018-08-18 12:05:18 -0700

    created back activity with navigation, color theme and confirmed WCAG 2.0 compliance for color-blindness accessibility

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-#Friend App
+#Filterable List App
 
 ##Overview
 This application is to show the power of filterable lists, using RecyclerView, Adapters and Fragments with multiple activities, views and passing/using Intent, layout and design in Android Studio.
 
-This app shows the five of the "friends" of my cats' Instagram account, @apartment_cats
-
+This app shows a list of Star Wars alien species and can be searched by species name as well as classification.
 ##Screenshots
 
 **Internal Editor View**
@@ -39,6 +38,8 @@ __Thanks to:__
 
 __Resources__
 
-- Open Source Data Sets to [choose from](https://github.com/awesomedata/awesome-public-datasets)
+- Data source from StarWars API [Species Data](https://swapi.co/api/species)
 
-- Final data source chosen from StarWars API [Species Data](https://swapi.co/api/species)
+- Confirmation of color-blindness accessability per [WCAG 2.0](http://www.w3.org/TR/WCAG20/) standards thanks to [WebAIM](https://webaim.org/resources/contrastchecker/) resources
+
+All images and references related to Star Wars are Copyright protected and property of the Walt Disney Corporation.
--- app/src/main/java/com/android/sooz/filterapp/models/SpeciesData.java
+++ app/src/main/java/com/android/sooz/filterapp/models/SpeciesData.java
@@ -9,7 +9,7 @@ public class SpeciesData {
 
 
     //private default constructor so no one else can add data
-    private SpeciesData(){
+    private SpeciesData() {
         species = new ArrayList<>();
         species.add(new StarWarsSpecies("Hutt", "gastropod", "Huttese", "https://swapi.co/api/planets/24/"));
         species.add(new StarWarsSpecies("Yoda's species", "mammal", "Galactic basic", "https://swapi.co/api/planets/28/"));
@@ -18,75 +18,41 @@ public class SpeciesData {
         species.add(new StarWarsSpecies("Ewok", "mammal", "Ewokese", "https://swapi.co/api/planets/7/"));
         species.add(new StarWarsSpecies("Sullustan", "mammal", "Sullutese", "https://swapi.co/api/planets/31/"));
         species.add(new StarWarsSpecies("Neimodian", "unknown", "Neimoidia", "https://swapi.co/api/planets/18/"));
-        
-    {
-        "name": "Gungan",
-            "classification": "amphibian",
-            "designation": "sentient",
-            "average_height": "190",
-            "skin_colors": "brown, green",
-            "hair_colors": "none",
-            "eye_colors": "orange",
-            "average_lifespan": "unknown",
-            "homeworld": "https://swapi.co/api/planets/8/",
-            "language": "Gungan basic",
-            "people": [
-        "https://swapi.co/api/people/36/",
-                "https://swapi.co/api/people/37/",
-                "https://swapi.co/api/people/38/"
-			],
-        "films": [
-        "https://swapi.co/api/films/5/",
-                "https://swapi.co/api/films/4/"
-			],
-        "created": "2014-12-19T17:30:37.341000Z",
-            "edited": "2014-12-20T21:36:42.163000Z",
-            "url": "https://swapi.co/api/species/12/"
-    },
-    {
-        "name": "Toydarian",
-            "classification": "mammal",
-            "designation": "sentient",
-            "average_height": "120",
-            "skin_colors": "blue, green, grey",
-            "hair_colors": "none",
-            "eye_colors": "yellow",
-            "average_lifespan": "91",
-            "homeworld": "https://swapi.co/api/planets/34/",
-            "language": "Toydarian",
-            "people": [
-        "https://swapi.co/api/people/40/"
-			],
-        "films": [
-        "https://swapi.co/api/films/5/",
-                "https://swapi.co/api/films/4/"
-			],
-        "created": "2014-12-19T17:48:56.893000Z",
-            "edited": "2014-12-20T21:36:42.165000Z",
-            "url": "https://swapi.co/api/species/13/"
-    },
-    {
-        "name": "Dug",
-            "classification": "mammal",
-            "designation": "sentient",
-            "average_height": "100",
-            "skin_colors": "brown, purple, grey, red",
-            "hair_colors": "none",
-            "eye_colors": "yellow, blue",
-            "average_lifespan": "unknown",
-            "homeworld": "https://swapi.co/api/planets/35/",
-            "language": "Dugese",
-            "people": [
-        "https://swapi.co/api/people/41/"
-			],
-        "films": [
-        "https://swapi.co/api/films/4/"
-			],
-        "created": "2014-12-19T17:53:11.214000Z",
-            "edited": "2014-12-20T21:36:42.167000Z",
-            "url": "https://swapi.co/api/species/14/"
+        species.add(new StarWarsSpecies("Gungan", "amphibian", "Gungan basic", "https://swapi.co/api/planets/8/"));
+        species.add(new StarWarsSpecies("Tovdarian", "mammal", "Toydarian", "https://swapi.co/api/planets/34/"));
+        species.add(new StarWarsSpecies("Dug", "mammal", "Dugese", "https://swapi.co/api/planets/35/"));
     }
-	]
+
+    //not sure, but I think this helps handle search filter
+    public static SpeciesData get(){
+        if(mSingleton == null) {
+            mSingleton = new SpeciesData();
+        }
+        return mSingleton;
+    }
+
+    //create a getter method to prevent the list from
+    // being set to null or another list
+    public List<StarWarsSpecies> species(){
+        return species;
+    }
+
+    //search method for the list to make it filterable
+    //eventually would like to pull in planet data as string and make searchable by planet too
+    //makes list searchable by species name and classification (mammal, reptile, etc.)
+    public static List<StarWarsSpecies> search(String filter){
+        List<StarWarsSpecies> results = new ArrayList<>();
+
+        for (StarWarsSpecies alien : get().species()) {
+            if (alien.name.toLowerCase().contains(filter)) {
+                results.add(alien);
+            } else if (alien.classification.toLowerCase().contains(filter)) {
+                results.add(alien);
+            }
+        }
+
+        return results;
+    }
 }
 
-}
+
