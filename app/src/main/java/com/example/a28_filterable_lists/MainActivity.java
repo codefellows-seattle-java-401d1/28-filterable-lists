4f8993c869c361989e214b85a3bac30fb17d3858
Amy Cohen <anbcohen@gmail.com>
2018-08-18 10:58:22 -0700

    added the activity_main design

--- app/src/main/java/com/example/a28_filterable_lists/MainActivity.java
+++ app/src/main/java/com/example/a28_filterable_lists/MainActivity.java
@@ -3,8 +3,54 @@ package com.example.a28_filterable_lists;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.a28_filterable_lists.models.Fabrics;
+
 public class MainActivity extends AppCompatActivity {
 
+    Fabrics[] fabrics = {
+            new Fabrics("Cait", "knit", "DBP", "Brushed Polyester/Spandex\n" +
+                    "\n" +
+                    "\"Double\" - means the fabric is brushed on both sides\n" +
+                    "\"Single\" - means fabric is brushed on one side, smooth on the other. \n" +
+                    "96% POLYESTER / 4% SPANDEX\n" +
+                    "58\"/60\"\" wide \n" +
+                    "Medium weight\n" +
+                    "4 - WAY stretch\n" +
+                    "Works very well for dresses, leggings, tops, etc.\n" +
+                    "CARE: Wash on cold, tumble dry"),
+            new Fabrics("Devon", "knit", "hacci sweater", "HACCI SWEATER KNIT\n" +
+                    "POLY/SPANDEX\n" +
+                    "If listing says “Brushed” means is has a brushed “fuzzy” texture to it\n" +
+                    "4-way stretch\n" +
+                    "Medium weight\n" +
+                    "58\" Wide\n" +
+                    "This fabric is very soft and cozy. Perfect for sweaters, tops and dresses. \n" +
+                    "CARE: wash cold, tumble dry or lay flat to dry"),
+            new Fabrics("Jamie", "knit", "DBP", "Brushed Polyester/Spandex\n" +
+                    "\n" +
+                    "\"Double\" - means the fabric is brushed on both sides\n" +
+                    "\"Single\" - means fabric is brushed on one side, smooth on the other. \n" +
+                    "96% POLYESTER / 4% SPANDEX\n" +
+                    "58\"/60\"\" wide \n" +
+                    "Medium weight\n" +
+                    "4 - WAY stretch\n" +
+                    "Works very well for dresses, leggings, tops, etc.\n" +
+                    "CARE: Wash on cold, tumble dry"),
+            new Fabrics("Maize", "knit", "DBP", "Brushed Polyester/Spandex\n" +
+                    "\n" +
+                    "\"Double\" - means the fabric is brushed on both sides\n" +
+                    "\"Single\" - means fabric is brushed on one side, smooth on the other. \n" +
+                    "96% POLYESTER / 4% SPANDEX\n" +
+                    "58\"/60\"\" wide \n" +
+                    "Medium weight\n" +
+                    "4 - WAY stretch\n" +
+                    "Works very well for dresses, leggings, tops, etc.\n" +
+                    "CARE: Wash on cold, tumble dry"),
+            new Fabrics("Serenity", "knit", "Viscose, Bamboo & Spandex", "4-way stretch\n" +
+                    "96% Viscose from Bamboo/4% spandex\n" +
+                    "58\"-60\" WIDE Very soft with a nice drape. This fabric works really well for tops, dresses, skirts, etc.\n" +
+                    "CARE: wash cold, tumble dry")
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
