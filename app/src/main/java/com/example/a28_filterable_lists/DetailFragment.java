4d93622bae0523f7e297de452d5f47d43c0a44a5
Amy Cohen <anbcohen@gmail.com>
2018-08-18 13:49:23 -0700

    added a header image

--- app/src/main/java/com/example/a28_filterable_lists/DetailFragment.java
+++ app/src/main/java/com/example/a28_filterable_lists/DetailFragment.java
@@ -16,12 +16,9 @@ public class DetailFragment extends Fragment{
 //    public TextView category;
 //    public TextView type;
 //    public TextView description;
-    public Button goBackButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_go_back);
 //
 //        name = findViewById(R.id.name_detail);
 //        category = findViewById(R.id.category_detail);
@@ -29,15 +26,6 @@ public class DetailFragment extends Fragment{
 //        description = findViewById(R.id.description_detail);
 //
         View view = inflater.inflate(R.layout.activity_detail, container, false);
-//        goBackButton = view.findViewById(R.id.goBack);
-//        goBackButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                getActivity().finish();
-//            }
-//        });
-
-
         return view;
 
     }
