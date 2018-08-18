0a06177cd875bab5e6d7f4b44afc407c360f5355
Amy Cohen <anbcohen@gmail.com>
2018-08-18 11:24:43 -0700

    had to change the activity_main design since I had something wrong with the height, still not listing anything. I have to work on the recycler view next I believe

--- app/src/main/java/com/example/a28_filterable_lists/MainActivity.java
+++ app/src/main/java/com/example/a28_filterable_lists/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.a28_filterable_lists;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.a28_filterable_lists.models.Fabrics;
 
 public class MainActivity extends AppCompatActivity {
 
+    public TextView name;
+    public TextView category;
+    public TextView type;
+
+
     Fabrics[] fabrics = {
-            new Fabrics("Cait", "knit", "DBP", "Brushed Polyester/Spandex\n" +
-                    "\n" +
+            new Fabrics("Cait", "knit", "DBP",
                     "\"Double\" - means the fabric is brushed on both sides\n" +
                     "\"Single\" - means fabric is brushed on one side, smooth on the other. \n" +
                     "96% POLYESTER / 4% SPANDEX\n" +
@@ -18,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
                     "4 - WAY stretch\n" +
                     "Works very well for dresses, leggings, tops, etc.\n" +
                     "CARE: Wash on cold, tumble dry"),
-            new Fabrics("Devon", "knit", "hacci sweater", "HACCI SWEATER KNIT\n" +
+            new Fabrics("Devon", "knit", "hacci sweater",
                     "POLY/SPANDEX\n" +
                     "If listing says “Brushed” means is has a brushed “fuzzy” texture to it\n" +
                     "4-way stretch\n" +
@@ -26,8 +31,7 @@ public class MainActivity extends AppCompatActivity {
                     "58\" Wide\n" +
                     "This fabric is very soft and cozy. Perfect for sweaters, tops and dresses. \n" +
                     "CARE: wash cold, tumble dry or lay flat to dry"),
-            new Fabrics("Jamie", "knit", "DBP", "Brushed Polyester/Spandex\n" +
-                    "\n" +
+            new Fabrics("Jamie", "knit", "DBP",
                     "\"Double\" - means the fabric is brushed on both sides\n" +
                     "\"Single\" - means fabric is brushed on one side, smooth on the other. \n" +
                     "96% POLYESTER / 4% SPANDEX\n" +
@@ -36,8 +40,7 @@ public class MainActivity extends AppCompatActivity {
                     "4 - WAY stretch\n" +
                     "Works very well for dresses, leggings, tops, etc.\n" +
                     "CARE: Wash on cold, tumble dry"),
-            new Fabrics("Maize", "knit", "DBP", "Brushed Polyester/Spandex\n" +
-                    "\n" +
+            new Fabrics("Maize", "knit", "DBP",
                     "\"Double\" - means the fabric is brushed on both sides\n" +
                     "\"Single\" - means fabric is brushed on one side, smooth on the other. \n" +
                     "96% POLYESTER / 4% SPANDEX\n" +
@@ -46,14 +49,20 @@ public class MainActivity extends AppCompatActivity {
                     "4 - WAY stretch\n" +
                     "Works very well for dresses, leggings, tops, etc.\n" +
                     "CARE: Wash on cold, tumble dry"),
-            new Fabrics("Serenity", "knit", "Viscose, Bamboo & Spandex", "4-way stretch\n" +
+            new Fabrics("Serenity", "knit", "Viscose",
+                    "4-way stretch\n" +
                     "96% Viscose from Bamboo/4% spandex\n" +
                     "58\"-60\" WIDE Very soft with a nice drape. This fabric works really well for tops, dresses, skirts, etc.\n" +
                     "CARE: wash cold, tumble dry")
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        category = findViewById(R.id.category);
+        type = findViewById(R.id.type);
     }
 }
