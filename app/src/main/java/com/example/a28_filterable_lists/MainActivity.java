bf57a93cdc3eee6c64e66293f4549a5b66b6f388
Amy Cohen <anbcohen@gmail.com>
2018-08-18 21:29:05 -0700

    added some additional styling to the activity files. added a border from a stackoverflow suggestion

--- app/src/main/java/com/example/a28_filterable_lists/MainActivity.java
+++ app/src/main/java/com/example/a28_filterable_lists/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a28_filterable_lists;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
