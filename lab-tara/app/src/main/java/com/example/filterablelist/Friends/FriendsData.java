61ddefdf3aeddf61d6265e01b561058a6c78b1e2
Tara Johnson <tara@madface.com>
2018-08-18 18:24:50 -0700

    Created Friend class and constructor as per demo.

---
+++ lab-tara/app/src/main/java/com/example/filterablelist/Friends/FriendsData.java
@@ -0,0 +1,26 @@
+package com.example.filterablelist.Friends;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FriendsData {
+    private static FriendsData mData;
+    private List<Friend> friends;
+
+    // Make the default constructor private so no one else can make one
+    private FriendsData() {
+        friends = new ArrayList<>();
+        friends.add(new Friend("Katy Perry", "@katyperry", "Love. Light."));
+        friends.add(new Friend("Justin Bieber", "@justinbieber", "Let\\'s make the world better. Join me on @bkstg at \\'justinbieber\\'. PURPOSE OUT NOW"));
+        friends.add(new Friend("Barack Obama", "@BarackObama","Dad, husband, President, citizen."));
+        friends.add(new Friend("Rihanna", "@rihanna","ProFilter Foundation back in stock @fentybeauty at https://t.co/6HarIz1NrP !!"));
+        friends.add(new Friend("Taylor Swift", "@taylorswift13", "The old Taylor can\\'t come to the phone right now."));
+    }
+
+    public static FriendsData get() {
+        if (mData == null) {
+            mData = new FriendsData();
+        }
+        return mData;
+    }
+}
