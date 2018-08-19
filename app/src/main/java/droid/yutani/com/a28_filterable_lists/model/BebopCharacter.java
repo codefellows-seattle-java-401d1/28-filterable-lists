d1633cefd490cc8eb0071fb9fa0f4b4a92efc3df
brandon-fenty <brandonfenty@gmail.com>
2018-08-19 16:49:18 -0700

    adds model

---
+++ app/src/main/java/droid/yutani/com/a28_filterable_lists/model/BebopCharacter.java
@@ -0,0 +1,11 @@
+package droid.yutani.com.a28_filterable_lists.model;
+
+public class BebopCharacter {
+    public String name;
+    public String desc;
+
+    public BebopCharacter (String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+}
