819a766c9d43b4d6e0e03db9cd2b4781664fc260
brandon-fenty <brandonfenty@gmail.com>
2018-08-19 17:00:36 -0700

    fixed container on template layout

--- app/src/main/java/droid/yutani/com/a28_filterable_lists/SecondActivity.java
+++ app/src/main/java/droid/yutani/com/a28_filterable_lists/SecondActivity.java
@@ -2,12 +2,13 @@ package droid.yutani.com.a28_filterable_lists;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_frag_template);
 
-
+        View container = findViewById(R.id.container);
     }
 }
